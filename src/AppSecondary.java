a481246defab77047ce63283a93f17b042691797
PanagiotisBatsikouras <t8160079@dias.aueb.gr>
2018-12-31 19:32:32 +0200

    test with build & run problem

--- src/AppSecondary.java
+++ src/AppSecondary.java
@@ -3,6 +3,14 @@ public class AppSecondary {
 
 	public static void main(String[] args) {
 		System.out.println("This is a Test for AppSecondary class!");
+		
+		DatabaseSecondary.test();
+		
+		InsertionSecondary.test();
+		
+		PrintingSecondary.test();
+		
+		ModificationSecondary.test();
 
 	}
 
